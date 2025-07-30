11ae5d6d9f1c1a43177bccb6fc5a313227db3cdc
Hugo Aparecido Gregorio de Almeida <hugoapga626@gmail.com>
2025-07-30 19:43:25 -0300

    Começo

---
+++ java-curso/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+   public static void main(String[] args){
+      // numeros inteiros = 100, 234345, 255, 120, 1
+      // byte => 8 bits => -128 a 127
+      // short => 16 bits => -32.768 a 32.767
+      // int => 2 bits => -2.147.483.648 a 2.147.483.647
+      // long => 64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+      // numeros decimais = 1.2, 3.4, 5.6, 7.8, 9.0
+      // 5.50
+      // float => 32 bits => precisão simples
+      // double => 64 bits => precisão dupla
+
+      // String => representa palavras e frases
+      // char => representa um unico caractere => a, b, c, d
+      // char meuChar = 'a';
+      // meuChar = "Hugo";
+
+      // boolean => true ou false
+
+      byte b = 100;
+      short s = 10000;
+      int i = 100000;
+      long l = 100000L;
+      float f = 10.5f;
+      double d = 20.5;
+      char c = 'A';
+      String str = "Hugo";
+      boolean bool = true;
+
+      if (bool) {
+         System.out.println("Verdadeiro");
+      } else {
+         System.out.println("Falso");
+      }
+   }
+}
